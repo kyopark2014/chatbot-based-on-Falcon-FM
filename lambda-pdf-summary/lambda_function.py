79ce49e83114d2f2d9e96a0c2bec51ac1d478587
ksdyb.park <ksdyb@amazon.com>
2023-07-14 16:51:05 +0900

    update

--- lambda-pdf-summary/lambda_function.py
+++ lambda-pdf-summary/lambda_function.py
@@ -22,7 +22,7 @@ class ContentHandler(LLMContentHandler):
 
     def transform_input(self, prompt: str, model_kwargs: dict) -> bytes:
         #input_str = json.dumps({'inputs': prompt, **model_kwargs})
-        input_str = json.dumps({'inputs': prompt, 'parameters': model_kwargs})
+        input_str = json.dumps({'inputs': prompt, "parameters": model_kwargs})
         return input_str.encode('utf-8')
       
     def transform_output(self, output: bytes) -> str:
